58bcfb17288dee938c3255eaa5cba33b7ff07f60
EC2 Default User <ec2-user@ip-172-31-33-222.ap-northeast-1.compute.internal>
2022-12-26 14:19:53 +0000

    reduce関数

--- environment/front-dev-tutorial-master/04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
+++ environment/front-dev-tutorial-master/04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
@@ -5,5 +5,17 @@ function tag(accu, curr) {
     return `${accu}<${curr}>`;
 }
 
-const result = strArry.reduce(tag, "");
+function reduce(arry, callback, defaultValue) {
+    let accu =  defaultValue;
+    
+    for(let i = 0; i < accu.length; i++){
+        let curr = arry[i];
+        accu = callback(accu, curr);
+    }
+    
+    return accu;
+}
+
+const result = reduce(strArry, tag, "");
 console.log(result);
+

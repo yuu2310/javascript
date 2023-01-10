7412f42fb449a7f832e37c5705a20a18b1cc0ac4
EC2 Default User <ec2-user@ip-172-31-33-222.ap-northeast-1.compute.internal>
2023-01-10 13:49:59 +0000

    モバイル用メニュー

--- environment/front-dev-tutorial-master/05_JavaScriptを実践で使う/050_モバイル用メニューを作成しよう/start/main.js
+++ environment/front-dev-tutorial-master/05_JavaScriptを実践で使う/050_モバイル用メニューを作成しよう/start/main.js
@@ -4,16 +4,22 @@ class MobileMenu{
     this.DOM.btn=document.querySelector('.mobile-menu__btn');
     this.DOM.container=document.querySelector('#global-container');
     this.eventType=this._getEventType();
-    //this._addEvent();
-  this.DOM.btn.addEventListener('click',this._toggle.bind(this));
+    this._addEvent();
 }
  
- 
-_toggle(){
+    _getEventType(){
+        const isTouchCapable = "ontouchstart" in window ||
+        (window.DocumentTouch && document instanceof DocumentTouch);
+    
+        return isTouchCapable ? "touchstart" : "click";
+    }
+    
+    _toggle(){
     this.DOM.container.classList.toggle('menu-open');
     }
- // _addEvent(){
-     //   this.DOM.btn.addEventListener('click',this._toggle.bind(this))
-   //}
+    
+    _addEvent(){
+        this.DOM.btn.addEventListener('click',this._toggle.bind(this))
+    }
 }
 new MobileMenu();
